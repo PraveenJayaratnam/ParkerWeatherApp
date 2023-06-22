560bd81b633abe47666e6d0c8f0bdb2c0a681773
PraveenShaun <praveen18.warner@gmail.com>
2023-06-22 10:10:58 +0530

    fix

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, debounceTime, distinctUntilChanged, map } from 'rxjs';
@@ -9,17 +10,18 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   currentWeather(location: string): Observable<any> {
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=3da02ff7c3024565964190500231006&q=${location}&aqi=no`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=3da02ff7c3024565964190500231006&q=${location}&aqi=no`;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return this.http.get<any[]>(apiUrl);
   }
 
   forcastWeather(location: string): Observable<any> {
-    const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=3da02ff7c3024565964190500231006&q=${location}&days=3&aqi=no&alerts=no`;
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=3da02ff7c3024565964190500231006&q=${location}&days=3&aqi=no&alerts=no`;
     return this.http.get<any[]>(apiUrl);
   }
 
   searchLocations(query: string): Observable<any[]> {
-    const apiUrl = `http://api.weatherapi.com/v1/search.json?key=3da02ff7c3024565964190500231006&q=${query}`;
+    const apiUrl = `https://api.weatherapi.com/v1/search.json?key=3da02ff7c3024565964190500231006&q=${query}`;
     return this.http.get<any[]>(apiUrl).pipe(
       debounceTime(300),
       distinctUntilChanged(),
